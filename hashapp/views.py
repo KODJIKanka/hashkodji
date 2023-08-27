826e7288503fc9eaa08d3a37a790f1333b5cb686
KODJI KANKA Paul <kodji14kanka06paul99@gmail.com>
2023-08-27 19:54:06 +0200

    mis a jour

--- hashapp/views.py
+++ hashapp/views.py
@@ -2,12 +2,24 @@ import hashlib
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+
+
+
+
 @csrf_exempt
 def calculate_hash(request):
+    emp = ""
+    hash_algorithms = {
+            'MD5': hashlib.md5,
+            'SHA-1': hashlib.sha1,
+            'SHA-256': hashlib.sha256,
+            'SHA-384': hashlib.sha384,
+            'SHA-512': hashlib.sha512
+        }
     if request.method == 'POST':
         data = request.POST.get('data')
         file = request.FILES.get('file')
-
+        
         hash_algorithms = {
             'MD5': hashlib.md5,
             'SHA-1': hashlib.sha1,
@@ -31,7 +43,7 @@ def calculate_hash(request):
                 hash_obj = algorithm_func(data.encode('utf-8'))
                 data_hash = hash_obj.hexdigest()
                 results[algorithm_name] = data_hash
-
-        return render(request, 'hashapp/result.html', {'results': results})
-
-    return render(request, 'hashapp/index.html')
+        emp = "value"
+        return render(request, 'hashapp/index.html', {'results': results})
+    teste = "testing"
+    return render(request, 'hashapp/index.html',{'vide' : emp , 'results' : hash_algorithms} )
